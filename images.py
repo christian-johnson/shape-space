2d50c9de4ade2a5ea8349143913e0bde0340e346
kwitophurr <arcjohns@ucsc.edu>
2018-03-27 14:29:13 -0700

    Added dottedP function to calculate correlation

--- images.py
+++ images.py
@@ -12,8 +12,10 @@ def dispMatrices(M1, M2):
     plt.imshow(M2)#, norm=cm.PowerNorm(gamma=2.6), vmin=M2.min(), vmax = M2.max())
     plt.show()
 
-#Define list of points that correspond to circle of radius R
 def radiiPoints(R):
+    """
+    Get ordered list of points that correspond to circle of radius R
+    """
     width = 2*R+1
     xspace = np.arange(width)-R
     yspace = np.arange(width)-R
@@ -23,3 +25,12 @@ def radiiPoints(R):
     ypts = np.nonzero((dist<R+0.5) & (dist>R-0.5))[1]-R
     order = np.argsort(np.arctan2(xpts, ypts))
     return xpts[order], ypts[order]
+
+def dottedP(P, R, i, j, k):
+    """
+    returns <P1|P2>(R) at pixel(i,j) with offset k
+    """
+    xpix, ypix = radiiPoints(R)
+    P1 = P[xpix+j, ypix+i]
+    P2 = P1[np.arange(-k, len(P1)-k)]
+    return np.cos(np.arccos(P1)-np.arccos(P2))
