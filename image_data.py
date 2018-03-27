77f427d120be112d3dc63bcc3d7962cdeaaea995
Adam Reyes <adam.c.reyes@gmail.com>
2018-03-27 16:16:48 -0700

    primitive version of transformation

--- image_data.py
+++ image_data.py
@@ -13,12 +13,20 @@ class image_data():
 
         self.fig, self.ax = plt.subplots()
         self.ax.axis('off')
+        self.ax.set_aspect(aspect=1)
         self.N = np.array(N)
         self.image = np.zeros((N[0],N[1]))             
         self.coord = [x, y]
 
+    ##################################################
+    ########## visualization #########################
+
     def show_img(self, **kwargs):
-        self.ax.imshow(self.image, **kwargs)
+        x, y = np.meshgrid(self.coord[0], self.coord[1])
+        self.ax.pcolor(x, y, self.image.T, **kwargs)
+
+    ##################################################
+    ########## image manipulation ####################
 
     def normalize(self):
         #renormalizes the image to between -1 & 1
@@ -40,3 +48,58 @@ class image_data():
                         self.image[i,j] += 1
         self.normalize()
         ################################
+
+    ##################################################
+    ############### Image transformations ############
+    def radiiPoints(self, R):
+        """
+        Get ordered list of points that correspond to circle of radius R
+        """
+        width = 2*R+1
+        xspace = np.arange(width)-R
+        yspace = np.arange(width)-R
+        xx, yy= np.meshgrid(xspace, yspace)
+        dist = np.sqrt(xx**2+yy**2)
+        xpts = np.nonzero((dist<R+0.5) & (dist>R-0.5))[0]-R
+        ypts = np.nonzero((dist<R+0.5) & (dist>R-0.5))[1]-R
+        order = np.argsort(np.arctan2(xpts, ypts))
+        return xpts[order], ypts[order]
+
+    def pt_in(self, x, y):
+        return (x >= 0) & (x < self.N[0]) & (y >= 0) & (y < self.N[1])
+
+    def dotted_P(self, R, i, j):
+        """
+        returns <P1|P2>(R) at pixel(i,j) with offset k
+        """
+        xpix , ypix  = self.radiiPoints(R)
+        k = int(len(xpix)/2)
+        #P1 = self.image[xpix , ypix ]
+        xpix2, ypix2 = xpix[np.arange(-k, len(xpix)-k)], ypix[np.arange(-k, len(ypix)-k)]
+        
+        xpair  = xpix [(self.pt_in(xpix+i,ypix+j) & self.pt_in(xpix2+i,ypix2+j))]
+        ypair  = ypix [(self.pt_in(xpix+i,ypix+j) & self.pt_in(xpix2+i,ypix2+j))]
+        xpair2 = xpix2[(self.pt_in(xpix+i,ypix+j) & self.pt_in(xpix2+i,ypix2+j))]
+        ypair2 = ypix2[(self.pt_in(xpix+i,ypix+j) & self.pt_in(xpix2+i,ypix2+j))]
+        
+        P1 = self.image[xpair , ypair ]
+        P2 = self.image[xpair2, ypair2]
+
+        temp =  (np.cos(np.arccos(P1[:k])-np.arccos(P2[:k])))
+        if len(temp) > 0 :
+            return np.sum(temp)/len(temp)
+        else :
+            return 0.
+
+    def transform_img(self, Rmax):
+        rr = np.arange(1,Rmax+1)
+        S =  np.zeros(Rmax)
+        for i in range(self.N[0]):
+            for j in range(self.N[1]):
+                # loop over pixels
+                for r in rr:
+                    #loop over
+                    rind = r - 1
+                    S[rind] += abs(self.dotted_P(r, i, j))
+        return(rr, S)
+          
--- testing.py
+++ testing.py
@@ -3,17 +3,27 @@ import matplotlib.pyplot as plt
 
 #import project specific dependencies
 import image_data as img
+from   images import *
 
 N = [20,20]
 xmin = [0.,0.]
 xmax = [1.,1.]
-x0 = [0.,0.]
-l, w = (.5,.6)
+x0 = [0.5,0.5]
+l, h = (.25,.25)
+delta = (xmax[0] - xmin[0] )/N[0]
+
+Rmax = int(min(N)/2)
 
 
 #fig, ax = plt.subplots()
 image = img.image_data(N,xmin,xmax)
-image.add_rect(x0, l, w)
+rr, norm = image.transform_img(Rmax)
+
+image.add_rect(x0, l, h)
 image.show_img(cmap='inferno')
+
+fig, ax = plt.subplots()
+rr, S = image.transform_img(Rmax)
+ax.plot(rr*delta,S-norm)
 
 plt.show()
