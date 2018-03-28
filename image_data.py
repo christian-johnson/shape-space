613445ef1f1cace3520f18cc9d8ccd050d7f3b12
Adam Reyes <adam.c.reyes@gmail.com>
2018-03-28 15:23:02 -0700

    updates

--- image_data.py
+++ image_data.py
@@ -81,23 +81,43 @@ class image_data():
     def pt_in(self, x, y):
         return (x >= 0) & (x < self.N[0]) & (y >= 0) & (y < self.N[1])
 
-    def dotted_P(self, R, i, j, k):
+    def get_pairs(self, xpix,ypix,k):
+        Nx = len(xpix)
+        xpair  = []
+        ypair  = []
+        xpair2 = []
+        ypair2 = []
+        xpix2, ypix2 = xpix[np.arange(-k, Nx-k)], ypix[np.arange(-k, Nx-k)]
+        for i in range(Nx):
+               if self.pt_in(xpix[i],ypix[i]) and self.pt_in(xpix2[i],ypix2[i]) :
+                   xpair .append(xpix [i])
+                   ypair .append(ypix [i])
+                   xpair2.append(xpix2[i])
+                   ypair2.append(ypix2[i])
+        ###############
+        return((xpair), (ypair), (xpair2), (ypair2))
+
+    def dotted_P(self, R, i, j, offset):
         """
         returns <P1|P2>(R) at pixel(i,j) with offset k
         """
         xpix , ypix  = self.radiiPoints(R)
+        N = len(xpix)
+        k = int(N/offset)
         N2 = int(len(xpix)/2)
         if k < 0 : k = N2
         xpix2, ypix2 = xpix[np.arange(-k, len(xpix)-k)], ypix[np.arange(-k, len(ypix)-k)]
-        
+        '''
         xpair  = xpix [(self.pt_in(xpix+i,ypix+j) & self.pt_in(xpix2+i,ypix2+j))]
         ypair  = ypix [(self.pt_in(xpix+i,ypix+j) & self.pt_in(xpix2+i,ypix2+j))]
         xpair2 = xpix2[(self.pt_in(xpix+i,ypix+j) & self.pt_in(xpix2+i,ypix2+j))]
         ypair2 = ypix2[(self.pt_in(xpix+i,ypix+j) & self.pt_in(xpix2+i,ypix2+j))]
-        
+        '''
+
+        xpair, ypair, xpair2, ypair2 = self.get_pairs(xpix+i,ypix+j,k)
         P1 = self.image[xpair , ypair ]
         P2 = self.image[xpair2, ypair2]
-
+        N2 = int(len(xpair)/2)
         temp = 1-np.abs(P1[:N2]-P2[:N2])
         #temp = np.cos(np.arccos(P1[:N2])-np.arccos(P2[:N2]))
         #temp = P1[:N2]*P2[:N2]
@@ -113,14 +133,21 @@ class image_data():
         '''
         rr = np.arange(1,Rmax+1)
         S =  np.zeros(Rmax)
-
+        count = np.zeros(Rmax)
         for i in range(self.N[0]):
             for j in range(self.N[1]):
                 # loop over pixels
                 for r in rr:
-                    #loop over
+                    #loop over r
                     rind = r - 1
-                    if (self.image[i,j] >= 0.):
-                        S[rind] += abs(self.dotted_P(r, i, j,offset))
+                    #if (self.image[i,j] >= 0.):
+                    if True :
+                    #if (i == self.N[0]/2-10 and j == self.N[1]/2-10) :
+                        temp = (self.dotted_P(r, i, j,offset))
+                        if temp > 0. : count[rind] += 1
+                        S[rind] += temp
+        ###############
+        for r in rr:
+            if (count[r-1] > 0): S[r-1] *= 1./count[r-1]
         return(rr, S)
           
